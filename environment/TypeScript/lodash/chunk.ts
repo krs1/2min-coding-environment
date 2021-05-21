b9e0e1e07d11de1888596e178d286d8d5a82bc89
krs1 <krs1@users.noreply.github.com>
2021-05-21 10:18:48 +0900

    lodash_chunk

---
+++ environment/TypeScript/lodash/chunk.ts
@@ -0,0 +1,32 @@
+import slice from './slice.ts'
+import toInteger from './toInteger.ts'
+
+function chunk(array:[], size:number = 1) {
+  //引数の値か0を選別
+  size = Math.max(toInteger(size), 0)
+
+  const length = array == null ? 0 : array.length
+  if (!length || size < 1) {
+    return []
+  }
+  let index = 0
+  let resIndex = 0
+
+  /*
+  lengthが5でsizeが2だったら
+  [1,2,3,4,5]/2=2
+  Math.ceilを通すと2(sizeが小数点を含んでいた場合を想定している？)
+  new Arrayで空の配列を生成2だったら[empty,empty]のようになるっぽい
+
+  実行すると[[1,2], [3,4], [5]]となる
+  */
+  
+  const result = new Array(Math.ceil(length / size))
+
+  while (index < length) {
+    result[resIndex++] = slice(array, index, (index += size))
+  }
+  return result
+}
+
+export default chunk
